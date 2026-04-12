b5f097280535a038fd51f2db13da6371bbfab659
Vincent LESNIAK <vincentlesniakpro@gmail.com>
2026-04-12 13:27:35 +0200

    Add: adding audit level CLI in ci.yml

--- test.js
+++ test.js
@@ -4,15 +4,10 @@ const { randFloatSpread } = require('./src/math.js');
 // exécute la fonction avec le paramètre 1 et stocke le résultat
 const resultat = randFloatSpread(1);
 
-// vérifie si le résultat est bien inférieur ou égal à 1
 if (resultat <= 1) {
-  // affiche un message de réussite dans le terminal
   console.log('Test passed');
-  // quitte le script
   process.exit(0);
 } else {
-  // affiche un message d'erreur dans le terminal
   console.error('Test Failed');
-  // quitte le script
   process.exit(1);
 }
