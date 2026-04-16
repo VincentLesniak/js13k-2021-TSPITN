6949b79c73c8fcedfc06053e9311ef57dce0762b
Vincent Lesniak <vincentlesniakpro@gmail.com>
2026-04-16 10:45:36 +0200

    add: fonctionnal tests

---
+++ cypress/e2e/jeu.cy.js
@@ -0,0 +1,36 @@
+describe('tests basiques du jeu', () => {
+
+  beforeEach(() => {
+    // charge le fichier local dans le navigateur virtuel
+    cy.visit('../../index.html')
+  })
+
+  it('verifie les elements principaux', () => {
+    // controle la visibilite de la zone de dessin
+    cy.get('canvas').should('be.visible')
+    
+    // valide la presence du mot clé dans le titre principal
+    cy.get('h1').should('contain', 'Two')
+  })
+
+  it('verifie les compteurs', () => {
+    // cherche la classe css correspondante au score
+    cy.get('.s').should('exist')
+    
+    // cherche la classe css correspondante aux points de vie
+    cy.get('.h').should('exist')
+  })
+
+  it('verifie la defaite', () => {
+    // assure que le menu de fin de partie est invisible au demarrage
+    cy.get('.e').should('be.hidden')
+  })
+
+  //définit la vérification impossible
+
+//   it('echoue en cherchant un fantome', () => {
+//     //cherche une classe css totalement imaginaire
+//     cy.get('.licorne-magique').should('exist')
+//   })
+
+})
